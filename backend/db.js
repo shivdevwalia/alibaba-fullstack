6edb8197061eb6fbdc6b6bcd371fea3307f18d67
shivdevwalia <shivdevsinghwalia@gmail.com>
2025-06-16 14:52:12 +0530

    added vercel file

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-require("dotenv").config(); 
+require("dotenv").config();
 const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/alibaba";
 const connection = mongoose.connect(MONGO_URI);
 
