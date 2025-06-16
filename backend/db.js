316366a7af33e798aaabb524b8cdbb7bac24f90e
shivdevwalia <shivdevsinghwalia@gmail.com>
2025-06-16 15:09:05 +0530

    Fix cors

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/alibaba";
+const MONGO_URI = process.env.MONGO_URI;
 const connection = mongoose.connect(MONGO_URI);
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ dotenv.config();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://alibaba-fullstack.onrender.com",
     credentials: true,
   })
 );
